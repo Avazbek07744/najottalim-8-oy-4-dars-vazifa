587cfff19ef873303330faaa31bd76c83b342460
Avazbek07744 <javazbek87@gmail.com>
2024-11-19 14:11:21 +0500

    vazifam

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import axios from "axios";
 import Cart from "./components/Card";
 import { regions } from "./data/regions";
@@ -9,47 +9,68 @@ interface DataType {
   description: string;
 }
 
-const App:FC = () => {
+const App: FC = () => {
   const [dataJson, setDataJson] = useState<DataType[]>([]);
+  const [selectedR, setSelectedR] = useState<string>("");
 
-  useEffect(() => {
-    const fetchWeather = async () => {
-      const apiKey = "3cc68d3216261b056230d71513da1450";
-      const data = await Promise.all(
-        regions.map(async (region) => {
-          const res = await axios.get(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${region.lat}&lon=${region.lon}&units=metric&appid=${apiKey}`
-          );
-          return {
-            name: region.name,
-            temperature: res.data.main.temp,
-            description: res.data.weather[0].description,
-          };
-        })
-      );
-      setDataJson(data);
+  const func = async (regionName: string) => {
+    const apiKey = "3cc68d3216261b056230d71513da1450";
+    const region = regions.find((mintaqa) => mintaqa.name === regionName);
+    if (!region) return;
+
+    const res = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${region.lat}&lon=${region.lon}&units=metric&appid=${apiKey}`
+    );
+
+    const viloyat = {
+      name: region.name,
+      temperature: res.data.main.temp,
+      description: res.data.weather[0].description,
     };
 
-    fetchWeather();
-  }, []);
+    setDataJson([viloyat]);
+  };
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const regionName = event.target.value;
+    setSelectedR(regionName);
+    func(regionName);
+  };
 
   return (
     <div className="p-8 bg-gradient-to-r from-blue-100 via-gray-100 to-blue-50 min-h-screen">
-    <h1 className="text-4xl font-extrabold text-blue-600 mb-6 text-center shadow-sm">
-      O'zbekiston Viloyatlari Ob-Havo
-    </h1>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {dataJson.map((weather) => (
-        <Cart
-          key={weather.name}
-          name={weather.name}
-          temperature={weather.temperature}
-          description={weather.description}
-        />
-      ))}
+      <h1 className="text-4xl font-extrabold text-blue-600 mb-6 text-center shadow-sm">
+        O'zbekiston Viloyatlari Ob-Havo
+      </h1>
+
+      <div className="mb-6 text-center">
+        <select
+          value={selectedR}
+          onChange={handleSelectChange}
+          className="p-3 border-2 border-blue-500 text-blue-500 outline-none rounded-md shadow-md text-xl"
+        >
+          <option value="" disabled>
+            Viloyatni tanlang
+          </option>
+          {regions.map((region) => (
+            <option key={region.name} value={region.name}>
+              {region.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="w-[500px] mx-auto mt-20">
+        {dataJson.map((value,index) => (
+          <Cart
+            key={index}
+            name={value.name}
+            temperature={value.temperature}
+            description={value.description}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-  
   );
 };
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,8 +6,10 @@ interface cartType {
     description: string;
 }
 
+
+
 const Cart: React.FC<cartType> = ({ name, temperature, description }) => (
-    <div className="border border-blue-300 bg-gradient-to-b from-white to-blue-50 p-6 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
+    <div className="cursor-pointer border border-blue-300 bg-gradient-to-b from-white to-blue-50 p-6 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300">
         <h2 className="text-2xl font-extrabold text-blue-600 mb-2">{name}</h2>
         <p className="text-xl text-gray-700 font-medium">
             Temperature: <span className="text-blue-500">{temperature}°C</span>
